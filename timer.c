cabda36acb5789989b9f3c6fd19682d7da4ade6e
Mathias Weyland <mathias@weyland.ch>
2013-06-08 14:37:07 +0200

    Change PWM generation from CTC to fast PWM mode.

--- timer.c
+++ timer.c
@@ -15,8 +15,8 @@ void initializeTimer(void){
 
 	TCCR1A = (1<<COM1A1) | (0<<COM1A0) | //Non-inverting mode
 		 (1<<COM1B1) | (0<<COM1B0);  //i.e. set on compare match, clear at bottom) 
-		 (1<<WGM11) | (0<<WGM10);     //mode 12, CTC mode
-	TCCR1B = (1<<WGM13) | (0<<WGM12) |
+		 (1<<WGM11) | (0<<WGM10);     //mode 14, fast PWM
+	TCCR1B = (1<<WGM13) | (1<<WGM12) |
 		 (0<<CS12) | (0<<CS11) | (1<<CS10); //No prescalar
 	TCCR1C = (0<<FOC1A) | (0<<FOC1B); //disable force output compare
 	TOCPMSA1 = (RIGHT_PWM_OUT1) | (LEFT_PWM_OUT1); //deciding output pwm pins
