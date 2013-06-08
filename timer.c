3d92aa185d4ca47eb41fb954d09564c3b15c17c5
Mathias Weyland <mathias@weyland.ch>
2013-06-08 14:47:07 +0200

    Explicit OC multiplexing to output pin for testing.

--- timer.c
+++ timer.c
@@ -19,8 +19,9 @@ void initializeTimer(void){
 	TCCR1B = (1<<WGM13) | (1<<WGM12) |
 		 (0<<CS12) | (0<<CS11) | (1<<CS10); //No prescalar
 	TCCR1C = (0<<FOC1A) | (0<<FOC1B); //disable force output compare
-	TOCPMSA1 = (RIGHT_PWM_OUT1) | (LEFT_PWM_OUT1); //deciding output pwm pins
-	TOCPMSA0 = (RIGHT_PWM_OUT0) | (LEFT_PWM_OUT0);
+    TOCPMSA0 = (0 << TOCC2S1) | (0 << TOCC2S0); // route OC0A to TOCC2
+//	TOCPMSA1 = (RIGHT_PWM_OUT1) | (LEFT_PWM_OUT1); //deciding output pwm pins
+//	TOCPMSA0 = (RIGHT_PWM_OUT0) | (LEFT_PWM_OUT0);
 
 	TCNT1 = 0x0000; //timer starts from 0
 	
